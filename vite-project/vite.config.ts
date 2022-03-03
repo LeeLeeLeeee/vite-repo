fc2c66a692a0621a92e4619617724ea1970afd69
LeeLeeLeeee <ijj1792@naver.com>
2022-03-03 23:44:38 +0900

    esbuild로 worker 설정

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -2,17 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import Inspect from 'vite-plugin-inspect'
-import replace from '@rollup/plugin-replace';
-
+import inlineWorkerPlugin from 'esbuild-plugin-inline-worker';
+console.log(inlineWorkerPlugin);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCommonjs(), Inspect(), {
-    ...replace({
-      preventAssignment: true,
-      __buildDate__: () => JSON.stringify(new Date()),
-      __buildVersion: '15',
-      '.worker': '.worker?worker'
-    }),
-    enforce: 'pre',
-  }],
+  plugins: [react(), viteCommonjs(), Inspect()],
+  optimizeDeps: {
+    include: ['sub'],
+    
+  }
 })
